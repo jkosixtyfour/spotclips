9ba5c525027ebe366a7204a36e333f30b02a488e
RiskDevKit <96445516+TheRealHyped@users.noreply.github.com>
2022-09-01 23:05:06 -0400

    skid shit
    did a bunch of shit ye

---
+++ header.php
@@ -0,0 +1,15 @@
+<div class="header">
+              <div class="nav">
+              <small><span style="text-align: right;"><!--Logged in as <b>jko64</b>--> sign in | sign up </span> | <a class="navlink" href=".">home</a> | <a class="navlink" href=".">users</a> | <a class="navlink" href=".">videos</a> | <a class="navlink" href=".">upload</a></small>
+              </div>
+              <div class="imgheader">
+              <img src="headerimg/retro.png">
+              <!-- homepage logo banner dimensions are 650x200 -->
+              </div>
+          <!--    <div class="extranav">
+              <p>this is extra navigation shit idk if this will stay</p>
+              </div>  -->
+              <div class="nav">
+                 <small>today is <?php echo date("F d, Y"); ?> | news | staff applications</small>
+              </div>
+            </div>
--- index.php
+++ index.php
@@ -5,32 +5,21 @@
         <?php include("ticker.php"); ?>
         <br>
         <div class="container">
-            <div class="header">
-              <div class="nav">
-              <small><span style="text-align: right;">Logged in as <b>jko64</b></span> | <a class="navlink" href=".">home</a> | <a class="navlink" href=".">users</a> | <a class="navlink" href=".">videos</a> | <a class="navlink" href=".">upload</a></small>
-              </div>
-              <div class="imgheader">
-              <img src="headerimg/retro.png">
-              <!-- homepage logo banner dimensions are 650x200 -->
-              </div>
-          <!--    <div class="extranav">
-              <p>this is extra navigation shit idk if this will stay</p>
-              </div>  -->
-              <div class="nav">
-                 <small>today is <?php echo date("F d, Y"); ?> | news | staff applications</small>
-              </div>
-            </div>
+            <?php include("header.php"); ?>
             <div class="main">
-               <h1>welcome to sc99</h1>
-               <p>sc99 is the least trustworthy website to share your videos!</p>
-               <p>you can:</p>
-               <ul>
-                <li>upload videos</li>
-                <li>comment on videos</li>
-                <li>share videos</li>
-                <li>and more...</li>
-               </ul>
-               <p>check out our discord server to learn more</p>
+                <div class="sidebar">
+                <h2>What's New</h2>
+                <p>RevTube is dead</p>
+                <hr>
+                <p>New, better looking UI.</p>
+                <hr>
+                <p>Started development!</p>
+                </div>
+                <div class="content">
+                <h2>Trending & Featured Videos</h2>
+                <small><b>free admin</b> - by <a href="/web/20211231224435/https://meb.trolled.lol/?id=8">jko64</a> <span style="float:right">2021-12-30 | <a href="view.php?v=tuCYH51yzDL"><small>[view]</small></a></span><hr></small>
+                <small><b>welcome to my site</b> - by <a href="/web/20211231224435/https://meb.trolled.lol/?id=8">admin</a> <span style="float:right">2017-05-16 | <a href="view.php?v=tuCYH51yzDL"><small>[view]</small></a></span><hr></small>
+            </div>
             </div>
             <div class="footer">
 
