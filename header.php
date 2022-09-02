e6bf30a591c95f108bf8765e3d40847cd1b19b87
RiskDevKit <96445516+TheRealHyped@users.noreply.github.com>
2022-09-01 23:09:01 -0400

    made the sign in/up things actually links
    also added some comments to the secondary nav

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div class="header">
               <div class="nav">
-              <small><span style="text-align: right;"><!--Logged in as <b>jko64</b>--> sign in | sign up </span> | <a class="navlink" href=".">home</a> | <a class="navlink" href=".">users</a> | <a class="navlink" href=".">videos</a> | <a class="navlink" href=".">upload</a></small>
+              <small><span style="text-align: right;"><!--Logged in as <b>jko64</b>--> <a href="#">sign in</a> | <a href="#">sign up</a> </span> | <a class="navlink" href=".">home</a> | <a class="navlink" href=".">users</a> | <a class="navlink" href=".">videos</a> | <a class="navlink" href=".">upload</a></small>
               </div>
               <div class="imgheader">
               <img src="headerimg/retro.png">
@@ -11,5 +11,6 @@
               </div>  -->
               <div class="nav">
                  <small>today is <?php echo date("F d, Y"); ?> | news | staff applications</small>
+                 <!-- here is supposed to be more links to stuff but not yet -->
               </div>
             </div>
