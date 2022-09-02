2785a8ad844c924de656a3616b3c8c85bd88524a
RiskDevKit <96445516+TheRealHyped@users.noreply.github.com>
2022-09-02 00:53:01 -0400

    barebones mobile layout
    just width `100 everything and do some line breaks easy

---
+++ mobile/head.php
@@ -0,0 +1,12 @@
+<head>
+        <title>sc99 - getvideoinfo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <meta name="description" content="share videos on a mediocre website that looks like it's straight out of 2004"/>
+        <meta name="author" content="jko64"/>
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="http://spotclips.ml">
+        <meta property="og:title" content="SC99">
+        <meta property="og:description" content="share videos on a mediocre website that looks like it's straight out of 2004">
+        <link rel="stylesheet" href="styles.css">
+    </head>
